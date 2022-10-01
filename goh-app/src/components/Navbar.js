c808528f0f864fda819f73638e7131fda5d0d29a
Haoyu Zhang <haoyuz4009@gmail.com>
2022-10-01 18:23:55 -0400

    Conditional Navigation Bar

--- goh-app/src/components/Navbar.js
+++ goh-app/src/components/Navbar.js
@@ -1,5 +1,6 @@
 
 //dependencies
+import { useAuthContext } from '../hooks/useAuthContext'
 import { Link } from "react-router-dom"
 import { useLogout } from '../hooks/useLogout'
 
@@ -8,20 +9,28 @@ import styles from './Navbar.module.css'
 
 export default function Nav() {
     const { logout } = useLogout()
-    
+    const { user } = useAuthContext()
 
     return (
         <nav className={styles.navbar}>
             <ul>
-                <li className={styles.title}>Goh App</li>
+                <li className={styles.title}><Link to ='/'>Goh App</Link></li>
                 
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/signup">Sign Up</Link></li>
-                <li><a onClick={ logout }> Logout</a></li>
-                <li><Link to="/accountInfo">Account</Link></li>
-
+                {!user && (
+                    <>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/signup">Sign Up</Link></li>
+                    </>
+                )}
+                
+                {user && (
+                    <>
+                        <li><a onClick={ logout }> Logout</a></li>
+                        {/*<li><Link to="/accountInfo">Account</Link></li>*/}
+                    </>
+                )}
             </ul>
-            
+           
         </nav>
     )
 }
--- goh-app/src/pages/home/Home.js
+++ goh-app/src/pages/home/Home.js
@@ -10,7 +10,11 @@ export default function Home() {
 
   return (
     <div>
-      Home
+      <h2>HOME</h2>
+      <p>Group and collaborative environments have struggled to coordinate and 
+credit the contributions done by the individuals of the group. Our goal is to deliver 
+        a web application that can bring together the functionality of a project task manager 
+        where users can communicate, collaborate, and track their project. Additionally, generating an analytical report of the contributions of each group member that can be viewed after the completion of the project.</p>
     </div>
   )
 }
