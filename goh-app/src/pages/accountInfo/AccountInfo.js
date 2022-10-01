f5b6b1e3c217f3c3f8902d648dc2bf0093572d6e
Yunhan Huang <yunhanhuang.work@gmail.com>
2022-10-01 18:01:33 -0400

    Account Info

--- goh-app/src/pages/accountInfo/AccountInfo.js
+++ goh-app/src/pages/accountInfo/AccountInfo.js
@@ -7,45 +7,46 @@ import styles from './AccountInfo.module.css'
 export default function AccountInfo() {
     const auth = getAuth();
     const user = auth.currentUser;
-
-    const [ email, setEmail ] = useState('');
-    const [ password, setPassword ] = useState('');
     const [ newDisplayName, setDisplayName ] = useState('');
 
-
-    function updateInfo() {
+    async function updateInfo() {
+      console.log('Updating');
       if (user) {
-        console.log(user.displayName);
-        console.log(newDisplayName);
-        updateProfile(user, {displayName: newDisplayName}).then(() => {
-          console.log("Display Name updated to", newDisplayName);
-        }).catch((error) => {
+        updateProfile(user, {displayName: newDisplayName})
+        .then(() => {
+          console.log(user.displayName);
+        })
+        .catch((error) => {
           console.log(error.message);
         });
-        console.log(user.displayName);
+        await user.reload();
+        user = auth.currentUser;
       }
     }
 
     function seeSomething() {
-      console.log(user);
+      console.log(user.displayName);
     }
     
     return (
         <div>
-          <form onSubmit={updateInfo}>
+          <form onSubmit={updateInfo} className={styles['signup-form']}>
             <h2>Account Information</h2>
 
             <label>
               <span>Display Name</span>
-              <input type = "text"
-                        onChange={(e)=>setDisplayName(e.target.value)}
-                        value={newDisplayName}
+              <input
+                    type = "text"
+                    onChange={(e)=>{
+                      setDisplayName(e.target.value)
+                    }}
+                    value={newDisplayName}
               />
             </label>
+
+
             {<button>Save</button>}
           </form>
-
-          {/* Test */}
           <button onClick={seeSomething}>Test</button>
         </div>
       )
