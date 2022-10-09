553a2eb485978996ee90fef51a8a8509e5cd7d66
Haoyu Zhang <haoyuz4009@gmail.com>
2022-10-09 16:42:31 -0400

    Update ProjectCreate.js

--- goh-app/src/pages/project/projectcreate/ProjectCreate.js
+++ goh-app/src/pages/project/projectcreate/ProjectCreate.js
@@ -3,41 +3,48 @@ import { useState } from 'react';
 import { getAuth } from 'firebase/auth'
 import { useCollection } from '../../../hooks/useCollection';
 import { useAddDoc } from '../../../hooks/useAddDoc'
+import { useProject } from '../../../hooks/useProject';
 import { v4 as uuid } from 'uuid';
 
 export default function Project() {
    const [projName, setProjName] = useState('');
    const [projDescr, setProjDescr] = useState('');
-   const { addDoc, error } = useAddDoc();
+   const { createProject, error } = useProject();
+   //const { addDoc, error } = useAddDoc();
    const user = getAuth().currentUser;
 
    //query error
-   const { documents: projects } = useCollection('projects',
-        ['projid','==',`users/${user.uid}/tokens/projid`] 
-   );
+//    const { documents: projects } = useCollection('projects',
+//         ['projid','==',`users/${user.uid}/tokens/projid`] 
+//    );
 
-   const { documents: lists } = useCollection(`users/${user.uid}/tokens`, null);
+//    const { documents: lists } = useCollection(`users/${user.uid}/tokens`, null);
 
     const handleSubmit = (event) => {
     event.preventDefault();
+    createProject(user.uid, projName, projDescr);
     
     //generate date and unique id for project
-    const projid = uuid();
-    const createdAt = new Date();
+    // const projid = uuid();
+    // const createdAt = new Date();
 
-    //create new collection structure
-    const project = {
-        projectid: projid,
-        ownerid : user.uid,
-        projName,
-        projDescr,
-        createdAt
-    }
+    // //create new collection structure
+    // const project = {
+    //     projectid: projid,
+    //     ownerid : user.uid,
+    //     projName,
+    //     projDescr,
+    //     createdAt
+    // }
     
-    addDoc(`projects`, projid, project)
-    addDoc( `users/${user.uid}/tokens`, projid, projid)
+    // const projtoken = {
+    //     projid : projid 
+    // }
+    
+    // addDoc(`projects`, projid, project)
+    // addDoc( `users/${user.uid}/tokens`, projid, projtoken)
 
-    console.log('project list:' , projects)
+    // console.log('project list:' , projects)
    }
 
 
