9b975714b9cf0cf821b78a5b2b5a24fffc103bd4
Ziyang Fang <fang296@purdue.edu>
2022-10-30 01:15:43 -0400

    simplified notification
    no display

--- goh-app/src/hooks/useNotification.js
+++ goh-app/src/hooks/useNotification.js
@@ -1,26 +1,19 @@
-import { async } from '@firebase/util'
 import { useState } from 'react'
 
-import { v4 as uuid } from 'uuid';
+import { 
+    doc,
+    getDoc,
+    setDoc,
+    } from "firebase/firestore"
+   
+import { firedb } from '../firebase/config';
 export const useNotification =()=>{
     const[error, setError] = useState('')
-    const createNoti = async(senderId, ReceiverId, message)=>{
+    const createNotification = async(
+        ReceiverId,
+        message
+        ) => {
         setError(null)
-        
-        //may delete the id of message
-        const messid = uuid();
-
-        if(!ReceiverId.exists()){
-            console.log('Wrong ID to receive message')
-        }
-        if (!message.exists()) {
-            console.log('No message detected')
-        }
-        try{
-            postMessage(message,senderId, ReceiverId)
-        } catch(error){
-            console.log('error when sending message')
-        }
     }
     return null
 }
