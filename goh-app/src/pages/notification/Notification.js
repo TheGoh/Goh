25e8d78a9dc63d56fceaf191f77e2463f553e81a
Haoyu Zhang <haoyuz4009@gmail.com>
2022-11-02 16:32:02 -0400

    scratch version of notification page

--- goh-app/src/pages/notification/Notification.js
+++ goh-app/src/pages/notification/Notification.js
@@ -1,38 +1,30 @@
 import { useState, useEffect } from 'react';
 import * as React from 'react';
 
-import { styled } from '@mui/material/styles';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItemText from '@mui/material/ListItemText';
-import Badge from '@mui/material/Badge';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 
 import { useDocument } from '../../hooks/useDocument';
 import { useAuthContext } from '../../hooks/useAuthContext';
-import { useCollection } from '../../hooks/useCollection';
 import { firedb } from '../../firebase/config';
 import {updateDoc, doc} from "firebase/firestore";
 
 
+
 export default function Notification() {
     //current text
-    const [curText, setcurText] = useState('');
- 
-    const [talkList,settalkList]=useState([])
-
-    //current user list
-    const [userData,setUserData]=useState([])
 
     // get task status
     const { user } = useAuthContext();
     const { documents: userDetail } = useDocument('users', user.uid );
     useEffect(() => {
       if(userDetail){
-        console.log(userDetail.my_message);
+        console.log(userDetail.my_message)
+        // userDetail.my_message.forEach(msg => {
+        //   console.log(msg.message)
+        // })
       }
-    });
+    },[userDetail]);
 
     const handleClear = async(e) => {
       e.preventDefault();
@@ -46,10 +38,21 @@ export default function Notification() {
 
     }
 
+    if (!userDetail) {
+      return <div> Loading... </div>
+    }
+
     return(
-      <Grid container sx={{marginTop: '200px'}} columns={1}>
+      
+      <div className="notify_box">
+            <div>Message Number: {userDetail.my_message.length}</div>
+            {
+              userDetail.my_message.length > 0 && userDetail.my_message.map(msg => (
+                <div className = "notify_box_text" key = {msg.Time}> {msg.message} </div>
+              ))
+            }
             <Button onClick={handleClear}>Clear Messages</Button>
-      </Grid>
+      </div>
       
     )
 
