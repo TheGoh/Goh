6fc69af0d457d7cc10f021c2238fcd92dabbcbd1
Haoyu Zhang <haoyuz4009@gmail.com>
2022-11-25 18:14:54 -0500

    Update AccountInfo.js

--- goh-app/src/pages/accountInfo/AccountInfo.js
+++ goh-app/src/pages/accountInfo/AccountInfo.js
@@ -1,54 +1,81 @@
-import { 
-  getAuth,       
+import {
+  updateProfile,
   updatePassword, 
   updateEmail, 
   reauthenticateWithCredential,
   EmailAuthProvider
 } from "firebase/auth";
 import React from "react";
-import TextField from '@mui/material/TextField';
 import { useAuthContext } from '../../hooks/useAuthContext'
 import { useState } from "react";
 import styles from './AccountInfo.module.css'
+import { storage, firedb } from '../../firebase/config';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { doc, getDoc, setDoc, updateDoc, deleteDoc, getDocs, query, where , collection} from "firebase/firestore"
 
 
 
 export default function AccountInfo() {
     const { user } = useAuthContext();
-    const [ newDisplayName, setDisplayName ] = useState('');
-    const [ newEmail, setEmail ] = useState('');
+    const [ newEmail, setEmail ] = useState(user.email);
     const [ newPassword, setPassword ] = useState('');
     const [ currPass, setCurrPass ] = useState('');
     const [ avatar, setAvatar ] = useState(null)
-    const [description, setDescription ] = useState('');
+    const [ description, setDescription ] = useState('');
     
-    const updateInfo = (event) => {
+    const updateInfo = async (event) => {
       event.preventDefault();
-      const credential = EmailAuthProvider.credential(
-        user.email, 
-        currPass
-      );
-      reauthenticateWithCredential(user, credential).then((cred) => {
-            //user = cred.user;  
-            updateEmail(user, newEmail).then(()=>{
-              console.log("email update success")
-            }).catch((error) => {
-              console.log("email update error")
-            });
-            updatePassword(user, newPassword).then(()=>{
-              console.log("pass update success")
-            }).catch((error) => {
-              console.log("pass update error")
-            });
+      const credential = EmailAuthProvider.credential(user.email, currPass);
+      reauthenticateWithCredential(user, credential).then(async () => {
+        const tempEmail = user.email;
+        updateEmail(user, newEmail)
+        .catch((error) => {
+          return alert(error.message)
+        });
+        await updatePassword(user, newPassword)
+        .catch((error) => {
+          updateEmail(user, tempEmail)
+          return alert(error.message)
+        });    
       })
-    };
+      .catch((error) => {
+        return alert("INVALID PASSWORD")
+      })
+      let imgURL = user.photoURL;
+      if (avatar) {
+        if (!avatar.type.includes('image')) {
+          alert("Please select a image")
+          return;
+        } else if (avatar.size > 100000) {
+          alert("Invalid File Size")
+          return;
+        } else {
+          const path = `UserProfileImage/${user.uid}/${avatar.name}`
+          const storageRef = ref(storage, path);
+          await uploadBytes(storageRef, avatar)
+          imgURL = await getDownloadURL(storageRef);
+        }       
+      }
+
+      const ref = doc(firedb, `users`, user.uid)
+      await updateDoc(ref, {
+        email: newEmail,
+        photoURL: imgURL
+      })
+
+      await updateProfile(user, {
+        photoURL: imgURL
+      })
+    }
+
+    if (!user) return <div> loading...</div>
     
     return (
         <div>
           <form className={styles['signup-form']}>
-            <h2>Account Information</h2>
+            <h2>{user.displayName} Account Settings</h2>
             <label>
-            <span>New Email</span>
+            <span>Email</span>
               <input
                     type = "email"
                     value={newEmail}
@@ -67,7 +94,7 @@ export default function AccountInfo() {
             <span>Update Photo</span>
               <input
                     type = "file"
-                    value={currPass}
+                    value={avatar}
                     onChange = {(e) => {
                       let selected = e.target.files[0]
                       setAvatar(selected)
@@ -93,7 +120,7 @@ export default function AccountInfo() {
               />
             </label>
             
-            {<button onClick={updateInfo}>Save</button>}
+            <button onClick={updateInfo}>Save</button>
           </form>
         </div>
       )
