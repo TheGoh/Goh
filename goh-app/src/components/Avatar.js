0fd304224e41f11316159581117e8c210ed1118b
Yunhan Huang <yunhanhuang.work@gmail.com>
2022-11-25 21:36:44 -0500

    redo

--- goh-app/src/components/Avatar.js
+++ goh-app/src/components/Avatar.js
@@ -1,11 +1,9 @@
 import './Avatar.css'
-import defAva from '../imgs/default-avatar.png'
 
 export default function Avatar({ src }) {
-    let real_src = src === null ? defAva : src;
     return (
         <div className = "avatar">
-            <img src={real_src} alt="user avatar" />
+            <img src={src} alt="user avatar" />
         </div>
     )
 }
