4c6bdad11d37cf8364f92a76cc1b9b1562103ef4
Yunhan Huang <yunhanhuang.work@gmail.com>
2022-12-02 14:56:15 -0500

    Project create heading

--- goh-app/src/pages/project/projectcreate/ProjectCreate.js
+++ goh-app/src/pages/project/projectcreate/ProjectCreate.js
@@ -83,6 +83,7 @@ export default function ProjectCreate() {
             <Grid container spacing={5}
                   columns={5}
                   sx={{width: '90%', margin: 'auto' }}>
+                <Grid item xs={5}  sx={{display: 'flex', justifyContent: 'flex-start'}} className={styles['heading']}><h1> All Projects</h1></Grid>
                 <Grid item xs={1}>
                     <Button variant="outlined" className={styles['project-grid-button']} onClick={handleClickOpen}>
                         <AddIcon fontSize="large"/>
