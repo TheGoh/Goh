e4959807f64f8c2a9a7d37d043430b4b82095c62
Yunhan Huang <yunhanhuang.work@gmail.com>
2022-12-01 22:01:18 -0500

    color adjustment and code cleanup

--- goh-app/src/pages/project/chat/Chat.js
+++ goh-app/src/pages/project/chat/Chat.js
@@ -100,48 +100,6 @@ export default function Chat() {
           />
           <IconButton onClick={handleSend}><SendIcon/></IconButton>
         </Box>
-        
-        {/* <div className={styles['chat-window']}>
-          <div className={styles['chat-header']}>
-            <p>Live Chat</p>
-          </div>
-          <div className={styles['chat-body']}>
-            <ScrollToBottom className={styles['message-container']}>
-              {chatLog.length > 0 && chatLog.map((msg) => {
-                return (
-                  <div
-                    className={styles['message']}
-                    id={user.uid === msg.senderId ? styles['other'] : styles['you']}
-                  >
-                    <div>
-                      <div className={styles['message-content']}>
-                        <p>{msg.message}</p>
-                      </div>
-                      <div className={styles['message-meta']}>
-                        <p id="time">{msg.createAt.toDate().toLocaleString()}: </p>
-                        <p id="author">{msg.senderName}</p>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </ScrollToBottom>
-          </div>
-      <div className={styles['chat-footer']}>
-        <input
-          type="text"
-          value={msg}
-          placeholder="Hey..."
-          onChange={(event) => {
-            setMsg(event.target.value);
-          }}
-          onKeyPress={(event) => {
-            event.key === "Enter" && handleSend();
-          }}
-        />
-        <button onClick={handleSend}>&#9658;</button>
-      </div>
-    </div> */}
     </Box>
     )
 }
